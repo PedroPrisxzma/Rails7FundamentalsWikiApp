7f1f69580927fbc6ed3f8512a12d91dc450f6b98
Pedro <pedro.costa@rakuten.com>
2024-07-03 14:42:16 +0900

    Added title to shown wiki posts, tested creating a post using the rails command line

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'wiki_posts/example'
   resources :wiki_posts
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   get 'welcome/about'
   get 'about', to: redirect('welcome/about')
   # Defines the root path route ("/")
