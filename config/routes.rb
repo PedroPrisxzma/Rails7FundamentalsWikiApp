e5abe82b5d8a693ab0a3af604041c0dfca0de571
Pedro <pedro.costa@rakuten.com>
2024-07-03 14:15:35 +0900

    about page added with a link to it from the index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'welcome/about'
+  get 'about', to: redirect('welcome/about')
   # Defines the root path route ("/")
   root "welcome#index"
 end
