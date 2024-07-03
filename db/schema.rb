f1c005789e949f98ce06c36b74d247e155f73373
Pedro <pedro.costa@rakuten.com>
2024-07-03 14:21:24 +0900

    updated Wikiposts with a title column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_17_065544) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_03_051719) do
   create_table "wiki_posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
