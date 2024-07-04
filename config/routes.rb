892a01bb680b38b3cba732ecfde81e9166e444df
Pedro <pedro.costa@rakuten.com>
2024-07-04 14:03:58 +0900

    small refactor on routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'wiki_posts/example'
-  resources :wiki_posts
-  get 'welcome/index'
+  namespace :wiki_posts do #This has to be custom because it is not a direct CRUD action
+    get 'example'
+  end
+  resources :wiki_posts #this expects CRUD actions
+
+  namespace :welcome do
+    get 'index'
+    get 'about'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'welcome/about'
   get 'about', to: redirect('welcome/about')
   # Defines the root path route ("/")
   root "welcome#index"
